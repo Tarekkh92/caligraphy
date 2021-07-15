a550c857c4002ac5c356f5b1e54af288462a6fcb
fardos <khalailyt@gmail.com>
2021-07-15 14:28:28 +0300

    first commit

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ $(document).ready(function(){
       });
   });
       
+  $(function() {
+    $('#WAButton').floatingWhatsApp({
+      phone: '+972523800546', //WhatsApp Business phone number International format-
+      //Get it with Toky at https://toky.co/en/features/whatsapp.
+      headerTitle: 'Chat with us on WhatsApp!', //Popup Title
+      popupMessage: 'Hello, how can we help you?', //Popup Message
+      showPopup: true, //Enables popup display
+      buttonImage: '<img src="img/whatsapp.svg" />', //Button Image
+      //headerColor: 'crimson', //Custom header color
+      //backgroundColor: 'crimson', //Custom background button color
+      position: "right"    
+    });
+  });
+  
